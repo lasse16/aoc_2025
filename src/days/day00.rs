af6fe9328877acdd1b4bc2336ac788427da18285
Lasse Haffke <lasse@haffke.me>
2025-12-05 19:35:45 +0100

    feat(template): add day template file

---
+++ src/days/day00.rs
@@ -0,0 +1,38 @@
+use crate::day::Day;
+pub struct Day00;
+
+impl Day for Day00 {
+    fn solve_part_one(&self, input: &str) -> String {
+        todo!()
+    }
+
+    fn solve_part_two(&self, input: &str) -> String {
+        todo!()
+    }
+}
+
+impl Day00 {
+    fn parse_instruction(&self, instruction: &str) -> todo!() {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example_input_parsing() {
+        todo!()
+    }
+
+    #[test]
+    fn test_example_input_running_part1() {
+        todo!()
+    }
+
+    #[test]
+    fn test_example_input_running_part2() {
+        todo!()
+    }
+}
