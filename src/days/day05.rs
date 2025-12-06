b83c6a293bd4c31bd9bc2ed035cfcbb08c51914a
Lasse Haffke <lasse@haffke.me>
2025-12-06 02:16:58 +0100

    feat(day05): add solution for part 1

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -5,7 +5,12 @@ pub struct Day05;
 
 impl Day for Day05 {
     fn solve_part_one(&self, input: &str) -> String {
-        todo!()
+        let (ranges, ids) = Day05.parse_input(input);
+        let count = ids
+            .iter()
+            .filter(|id| ranges.iter().any(|range| range.contains(id)))
+            .count();
+        format!("{}", count)
     }
 
     fn solve_part_two(&self, input: &str) -> String {
@@ -14,8 +19,28 @@ impl Day for Day05 {
 }
 
 impl Day05 {
-    fn parse_instruction(&self, input: &str) -> (Vec<Range<u32>>, Vec<u32>) {
-        todo!()
+    fn parse_input(&self, input: &str) -> (Vec<Range<u64>>, Vec<u64>) {
+        let mut ranges = vec![];
+        let mut ids = vec![];
+        let (raw_ranges, raw_ids) = input
+            .split_once("\n\n")
+            .expect("No blankline found in input string");
+
+        for mut line in raw_ranges.lines() {
+            line = line.trim();
+            let (start, end) = line.split_once("-").unwrap();
+            let start: u64 = start.parse().unwrap();
+            let end: u64 = end.parse().unwrap();
+            ranges.push(start..end + 1);
+        }
+
+        for mut line in raw_ids.lines() {
+            line = line.trim();
+            let id: u64 = line.parse().unwrap();
+            ids.push(id);
+        }
+
+        (ranges, ids)
     }
 }
 
@@ -36,12 +61,16 @@ mod test {
 
     #[test]
     fn test_example_input_parsing() {
-        todo!()
+        let known_ranges = [3..6, 10..15, 16..21, 12..19];
+        let known_ids = [1, 5, 8, 11, 17, 32];
+        let (parsed_ranges, parsed_ids) = Day05.parse_input(EXAMPLE_INPUT);
+        assert_eq!(known_ranges, parsed_ranges.as_slice());
+        assert_eq!(known_ids, parsed_ids.as_slice());
     }
 
     #[test]
     fn test_example_input_running_part1() {
-        todo!()
+        assert_eq!(Day05.solve_part_one(EXAMPLE_INPUT), "3");
     }
 
     #[test]
